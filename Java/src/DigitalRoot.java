8ff1f527cc3bdb4b6efb5b253f2f3e304d996d2d
JabBerry <33551495+PaweenJabberry@users.noreply.github.com>
2020-03-06 17:43:00 +0700

    Update v1.9.1
    - DigitalRoot (Throw Exception)

--- Java/src/DigitalRoot.java
+++ Java/src/DigitalRoot.java
@@ -6,25 +6,33 @@
 //   Input : 12345
 //   Output : 6 (Because 1 + 2 + 3 + 4 + 5 equals 15 and then 1 + 5 equals 6)
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DigitalRoot {
-    public static void DigitalRoot(String line) {
+    public static void DigitalRoot(long num) {
+
+        if (num<=0) {
+            throw new InputMismatchException();
+        }
+        String line = String.valueOf(num);
         while (line.length()!=1) {
-            int currentValue = 0;
+            long currentValue = 0;
             for (int i=0;i<line.length();i++) {
-                currentValue += Character.getNumericValue(line.charAt(i));
+                if(Character.isDigit(line.charAt(i))) {
+                    currentValue += Character.getNumericValue(line.charAt(i));
+                }
             }
             line = String.valueOf(currentValue);
         }
 
-        System.out.println("Output : "+line);
+        System.out.println("Output : "+Long.parseLong(line));
     }
 
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         System.out.print("Input : ");
-        String line = s.nextLine();
-        DigitalRoot(line);
+        long num = s.nextLong();
+        DigitalRoot(num);
     }
 }
